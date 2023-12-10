d7d9dbae5a011af58050411e273cf09e01ae4b71
callbackKid <vladislav.yarosh23@gmail.com>
2023-12-11 01:18:57 +0400

    boiler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
+
   return (
     <div className="App">
-      HELLO WORLD
-      <h1>Hello</h1>
+
     </div>
   )
 }
---
+++ src/utils/toDos.js
@@ -0,0 +1,37 @@
+export const todos = [
+  {
+    taskTitle: 'Task 1',
+    taskDescription: 'Complete project presentation',
+    doneFlag: false,
+  },
+  {
+    taskTitle: 'Task 2',
+    taskDescription: 'Buy groceries',
+    doneFlag: true,
+  },
+  {
+    taskTitle: 'Task 3',
+    taskDescription: 'Call mom',
+    doneFlag: false,
+  },
+  {
+    taskTitle: 'Task 4',
+    taskDescription: 'Go for a jog',
+    doneFlag: true,
+  },
+  {
+    taskTitle: 'Task 5',
+    taskDescription: 'Read a book',
+    doneFlag: false,
+  },
+  {
+    taskTitle: 'Task 6',
+    taskDescription: 'Finish coding assignment',
+    doneFlag: true,
+  },
+  {
+    taskTitle: 'Task 7',
+    taskDescription: 'Pay bills',
+    doneFlag: false,
+  },
+]
